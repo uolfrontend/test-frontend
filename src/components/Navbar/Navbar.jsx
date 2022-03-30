e8da87c87cf96e978621d4aeb6451c9603011efe
Bruno Moreno <brunomoreno7538@gmail.com>
2022-03-29 23:00:34 -0300

    refactor: remove container component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,13 @@
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 import logo from '../../../public/logo.png';
 import './styles.css';
 
 const NavBar = () => {
   return (
-    <Navbar>
-      <Container className="justify-content-center">
-        <Navbar.Brand>
-          <img src={logo} height="30" className="d-inline-block align-top" />
-        </Navbar.Brand>
-      </Container>
+    <Navbar className="justify-content-center">
+      <Navbar.Brand>
+        <img src={logo} height="30" className="d-inline-block align-top" />
+      </Navbar.Brand>
     </Navbar>
   );
 };
