cc46fc30569418ac24ad548b08f4bb27cb60b4ab
Bruno Moreno <brunomoreno7538@gmail.com>
2022-03-31 03:10:39 -0300

    refactor: make routes div as main

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
+
+import Home from './components/Home/Home';
 
 const AppRoutes = () => {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 };
 
