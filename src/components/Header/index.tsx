44c39d679c0be3854e50747bd7f7fab84597fe14
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-04-19 17:07:30 -0300

    ✨ create header component and apply globally

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import {Box, Flex} from '@chakra-ui/react';
+import Image from 'next/image';
+import React from 'react';
+import logo from '@public/assets/images/logo.png';
+
+export default function Header() {
+  return (
+    <Flex h="60px" w="100%" bg="darkest" justify="center" py="10px">
+      <Image
+        src={logo}
+        alt="UOL - Universo Online"
+        width="100%"
+        height="100%"
+        objectFit="contain"
+      />
+    </Flex>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import {Button} from '@chakra-ui/react';
 import React from 'react';
 
 export default function Home() {
-  return <div>index</div>;
+  return <></>;
 }
