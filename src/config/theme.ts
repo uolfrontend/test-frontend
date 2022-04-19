572c1af1b3796d56f99c4e4949055ad2d6fd2c8b
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-04-19 17:06:22 -0300

    🎨 extends chakra theme

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,5 +1,48 @@
 import {extendTheme} from '@chakra-ui/react';
 
-const theme = extendTheme({});
+const theme = extendTheme({
+  colors: {
+    darkest: '#333333',
+    primary: '#969696',
+    secondary: '#e29933',
+  },
+  fonts: {
+    heading: "'Libre Franklin', sans-serif;",
+    body: "'Libre Franklin', sans-serif;",
+  },
+  components: {
+    Button: {
+      variants: {
+        primary: {
+          bg: 'secondary',
+          color: '#FFFFFF',
+          py: '20px',
+          px: '30px',
+          fontSize: '16px',
+          borderRadius: '5px',
+          _hover: {
+            bg: 'secondary',
+          },
+          _focus: {boxShadow: 'none'},
+        },
+        secondary: {
+          bg: 'white',
+          color: 'secondary',
+          border: '1px',
+          borderColor: 'secondary',
+          py: '20px',
+          px: '30px',
+          fontSize: '16px',
+          borderRadius: '5px',
+          _hover: {
+            bg: 'secondary',
+            color: 'white',
+          },
+          _focus: {boxShadow: 'none'},
+        },
+      },
+    },
+  },
+});
 
 export default theme;
