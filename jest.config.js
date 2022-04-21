9c2ebee0ec64d596aac60668d8fecb24975c1e72
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-04-21 19:34:11 -0300

    ✅ add user form tests

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     'components/(.*)': ['<rootDir>/src/components/$1'],
-    // 'utils/(.*)': ['<rootDir>/src/utils/$1'],
+    // 'utils/(.*)': ['<rootDir>/src/utils/$1'],x
   },
 };
 
