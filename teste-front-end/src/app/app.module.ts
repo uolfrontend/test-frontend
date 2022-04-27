fb9c2743e91e479602bfb456fa9009bfb9e8e9fe
Liara Pereira Duarte <lipereira@uolinc.com>
2022-04-26 21:37:24 -0300

    feature: Cria painel de clientes

--- teste-front-end/src/app/app.module.ts
+++ teste-front-end/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ClientPanelComponent } from './client-panel/client-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ClientPanelComponent
   ],
   imports: [
     BrowserModule
