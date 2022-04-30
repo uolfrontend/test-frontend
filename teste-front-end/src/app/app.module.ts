13ec200f1917940f4664aac468e6c1dc27ae80ec
Liara Pereira Duarte <lipereira@uolinc.com>
2022-04-30 13:35:31 -0300

    feature: Adiciona rotas

--- teste-front-end/src/app/app.module.ts
+++ teste-front-end/src/app/app.module.ts
@@ -1,26 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ClientPanelComponent } from './client-panel/client-panel.component';
 import { ListUsersComponent } from './list-users/list-users.component';
 import { ButtonComponent } from './button/button.component';
-import { HttpClientModule } from '@angular/common/http';
+import { NewUserComponent } from './new-user/new-user.component';
 
+const routes: Routes = [
+  { path: 'home', component: ListUsersComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'edit', component: NewUserComponent },
+]
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ClientPanelComponent,
     ListUsersComponent,
-    ButtonComponent
+    ButtonComponent,
+    NewUserComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule
+    BrowserModule,    
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
